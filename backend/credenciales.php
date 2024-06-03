d8865924fd6a0c5591c7c4098ab4b6f26f1df66b
juanjomrtz19 <jjmartagui@gmail.com>
2024-06-04 00:14:42 +0200

    cambios en credenciales para que sea mas facil

--- backend/credenciales.php
+++ backend/credenciales.php
@@ -4,9 +4,9 @@
 //$host = "localhost";
 //$database = "gran_hotel";
 
-
-$username = "root";
-$password = "";
+$username = "albertosm162324";
+$password = "DX72hsDerGZEJwS1";
 $host = "localhost";
-$database = "gran_hotel";
+$database = "albertosm162324";
+
 

3719ce0268d390bb71d0a48594e82c3fed40824c
AlbertoSM16 <albertosiles2001@gmail.com>
2024-06-05 00:19:03 +0200

    wip

--- backend/credenciales.php
+++ backend/credenciales.php
@@ -1,12 +1,8 @@
 <?php
-$username = "root";
-$password = "";
-$host = "localhost";
-$database = "gran_hotel";
 
-//$username = "albertosm162324";
-//$password = "DX72hsDerGZEJwS1";
-//$host = "localhost";
-//$database = "albertosm162324";
+$username = "albertosm162324";
+$password = "DX72hsDerGZEJwS1";
+$host = "localhost";
+$database = "albertosm162324";
 
 
